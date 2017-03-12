c7c0c1a396d103021d02efdff79b89b72245940b
Thomas Shipley <thomas.shipley@just-eat.com>
2017-03-12 18:50:43 +0000

    Added a simple healthcheck endpoint

---
+++ src/CodingMonkey.CodeExecutor/Controllers/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿namespace CodingMonkey.CodeExecutor.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class HealthCheckController : Controller
+    {
+        // GET: /<controller>/
+        public JsonResult Index()
+        {
+            return Json(new
+            {
+                OK = true
+            });
+        }
+    }
+}
