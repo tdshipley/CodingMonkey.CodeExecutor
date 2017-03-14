7b48176dd00ddf3b90f54f905a2b967c84137a1b
Thomas Shipley <thomas.shipley@just-eat.com>
2017-03-14 14:53:02 +0000

    Type conversion was used by the test executor to convert from often string representations of a value stored in a object to the concrete type.\

--- src/CodingMonkey.CodeExecutor/CodeExecution/TestExecutor.cs
+++ src/CodingMonkey.CodeExecutor/CodeExecution/TestExecutor.cs
@@ -1,10 +1,11 @@
-﻿namespace CodingMonkey.CodeExecutor.CodeExecution
+﻿using Serilog;
+
+namespace CodingMonkey.CodeExecutor.CodeExecution
 {
     using CodingMonkey.Models;
     using CodingMonkey.CodeExecutor.Models;
     using Structs;
     using System;
-    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public static class TestExecutor
@@ -12,23 +13,27 @@
         public static async Task RunTest(string code, CodeTemplate template, Test test, ResultSummary resultSummary)
         {
             var compiler = new RoslynCompiler();
-            if (!ConvertInputValueToValueType(test.Inputs))
-            {
-                resultSummary.ErrorProcessingTests = true;
-            }
-            else
+
+            try
             {
                 ExecutionResult result = await compiler.SanitiseCodeAndExecuteAsync(code, template.ClassName, template.MainMethodName, test.Inputs);
                 ProcessTestResult(result, test, resultSummary);
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Error(e, "Failed to run test");
+                resultSummary.ErrorProcessingTests = true;
+                throw;
             }
         }
 
         private static void ProcessTestResult(ExecutionResult result, Test testRun, ResultSummary resultSummary)
         {
+            resultSummary.CompilerErrors = null;
+            resultSummary.HasCompilerErrors = false;
+
             if (!result.Successful)
             {
-                resultSummary.CompilerErrors = null;
-                resultSummary.HasCompilerErrors = false;
                 resultSummary.HasRuntimeError = true;
 
                 resultSummary.RuntimeError = new RuntimeError()
@@ -39,133 +44,10 @@
             }
 
             testRun.ActualOutput = result.Value;
-            if (!AddTestResult(testRun))
-            {
-                resultSummary.ErrorProcessingTests = true;
-            }
-        }
+            testRun.Result = new TestResult { TestExecuted = true };
 
-        private static bool ConvertInputValueToValueType(List<TestInput> testInputs)
-        {
-            foreach (var input in testInputs)
-            {
-                switch (input.ValueType)
-                {
-                    case "String":
-                        {
-                            return GetValueFromTestInput<string>(input);
-                        }
-                    case "Integer":
-                        {
-                            return GetValueFromTestInput<int>(input);
-                        }
-                    case "Boolean":
-                        {
-                            return GetValueFromTestInput<bool>(input);
-                        }
-                    default:
-                        {
-                            return false;
-                        }
-                }
-            }
-
-            return false;
-        }
-
-        private static bool GetValueFromTestInput<T>(TestInput input)
-        {
-            object value = GetValue<T>(input.Value);
-            if (value == null)
-            {
-                return false;
-            }
-            else
-            {
-                input.Value = (T)value;
-                return true;
-            }
-        }
-
-        private static bool AddTestResult(Test test)
-        {
-            test.Result = new TestResult { TestExecuted = true };
-
-            switch (test.ExpectedOutput.ValueType)
-            {
-                case "String":
-                    {
-                        return AddResultToTestResult<string>(test);
-                    }
-                case "Integer":
-                    {
-                        return AddResultToTestResult<int>(test);
-                    }
-                case "Boolean":
-                    {
-                        return AddResultToTestResult<bool>(test);
-                    }
-                default:
-                    {
-                        return false;
-                    }
-            }
-        }
-
-        private static bool AddResultToTestResult<T>(Test test)
-        {
-            try
-            {
-                var value = GetValue<T>(test.ExpectedOutput.Value);
-                test.ExpectedOutput.Value = (T)value;
-
-                test.Result.TestPassed = value.Equals((T)test.ActualOutput);
-
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-
-        private static object GetValue<T>(object value)
-        {
-            switch (typeof(T).ToString())
-            {
-                case "System.String":
-                    {
-                        return value.ToString();
-                    }
-                case "System.Int32":
-                    {
-                        int inputValue;
-                        bool success = int.TryParse(value.ToString(), out inputValue);
-
-                        if (success)
-                        {
-                            return inputValue;
-                        }
-
-                        return null;
-                    }
-                case "System.Boolean":
-                    {
-                        bool inputValue;
-                        bool success = bool.TryParse(value.ToString(), out inputValue);
-
-                        if (success)
-                        {
-                            return inputValue;
-                        }
-
-                        return null;
-                    }
-                default:
-                    {
-                        return null;
-                    }
-            }
+            testRun.ExpectedOutput.Value = testRun.ExpectedOutput.Value.ToString();
+            testRun.Result.TestPassed = testRun.ExpectedOutput.Value.ToString().Equals(testRun.ActualOutput.ToString());
         }
     }
 }
