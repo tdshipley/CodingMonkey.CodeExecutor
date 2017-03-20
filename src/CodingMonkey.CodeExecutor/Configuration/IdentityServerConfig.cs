985484dd1c2b95fca3bf8add9dd2b11eeb910b07
Thomas Shipley <thomas.shipley@just-eat.com>
2017-03-20 17:42:24 +0000

    Add a healthcheck that the service can find identity server

---
+++ src/CodingMonkey.CodeExecutor/Configuration/IdentityServerConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace CodingMonkey.CodeExecutor.Configuration
+{
+    public class IdentityServerConfig
+    {
+        public string Authority { get; set; }
+        public string ScopeName { get; set; }
+        public string ScopeSecret { get; set; }
+    }
+}
